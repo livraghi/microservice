cd217fae7ca9b7d65abb11b4c05e418d08f51959
Simone Livraghi <simone.livraghi@gmail.com>
2024-02-16 23:41:25 +0100

    Start managing http server graceful shutdown

--- microservice.go
+++ microservice.go
@@ -2,35 +2,52 @@ package microservice
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"log/slog"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func StartDefaultHttpServer(ctx context.Context, port int32) {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	mainCtx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.DefaultServeMux.ServeHTTP(w, r)
+			ctx := r.Context()
+			for {
+				select {
+				case <-ctx.Done():
+					slog.Info("handler graceful shutdown")
+					w.WriteHeader(http.StatusServiceUnavailable)
+					return
+				}
+			}
+		}),
+		BaseContext: func(_ net.Listener) context.Context {
+			return mainCtx
+		},
 	}
 
-	g, gCtx := errgroup.WithContext(ctx)
+	g, gCtx := errgroup.WithContext(mainCtx)
 	g.Go(func() error {
 		return srv.ListenAndServe()
 	})
-
 	g.Go(func() error {
 		<-gCtx.Done()
 		return srv.Shutdown(gCtx)
 	})
 
-	if err := g.Wait(); err != nil {
-		slog.Error("http server crash", "error", err.Error())
+	if err := g.Wait(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("http server failed", "error", err.Error())
+	} else {
+		slog.Info("http server stopped")
 	}
 }
