8851bbb8a446d66c6463b28b1ac30fc5652325cd
Simone Livraghi <simone.livraghi@gmail.com>
2024-03-24 12:24:16 +0100

    Start opentelemetry instrumentation

--- examples/rest-gin/main.go
+++ examples/rest-gin/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"github.com/livraghi/microservice/observability"
 	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/livraghi/microservice"
 	"github.com/livraghi/microservice/configuration"
@@ -22,9 +26,24 @@ func main() {
 		configuration.WithConfigType(configuration.ENV),
 	)
 	if err != nil {
-		slog.Error("failed to load configurations", "error", err.Error())
+		slog.Error(
+			"failed to load configurations", "error", err.Error())
 		return
 	}
+
+	stopObservabilityFn, err := observability.SetUpObservability(cfg.Name, cfg.Version)
+	if err != nil {
+		slog.Error("failed to setup observability", "error", err.Error())
+		return
+	}
+	defer func() { _ = stopObservabilityFn(ctx) }()
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	http.Handle("/", r)
 
 	microservice.StartDefaultHttpServer(ctx, microservice.WithConfiguration(cfg))
 }
